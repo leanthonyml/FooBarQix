8a8efa99e3a486d16dcf0f69aff6ea1c87780f02
leanthonyml <40511671+leanthonyml@users.noreply.github.com>
2018-06-23 20:00:16 -0400

    Add files via upload

---
+++ TestClass.cs
@@ -0,0 +1,148 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FooBarQix
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TranslateNumber1()
+        {
+            Translator translator = new Translator();
+            Assert.AreEqual("1", translator.Translate("1"));
+        }
+
+        [Test]
+        public void TranslateNumber2()
+        {
+            Translator translator = new Translator();
+            Assert.AreEqual("2", translator.Translate("2"));
+        }
+
+        [Test]
+        public void TranslateNumber3()
+        {
+            Translator translator = new Translator();
+            Assert.AreEqual("FooFoo", translator.Translate("3"));
+        }
+
+        [Test]
+        public void TranslateNumber4()
+        {
+            Translator translator = new Translator();
+            Assert.AreEqual("4", translator.Translate("4"));
+        }
+
+        [Test]
+        public void TranslateNumber5()
+        {
+            Translator translator = new Translator();
+            Assert.AreEqual("BarBar", translator.Translate("5"));
+        }
+
+        [Test]
+        public void TranslateNumber6()
+        {
+            Translator translator = new Translator();
+            Assert.AreEqual("Foo", translator.Translate("6"));
+        }
+
+        [Test]
+        public void TranslateNumber7()
+        {
+            Translator translator = new Translator();
+            Assert.AreEqual("QixQix", translator.Translate("7"));
+        }
+
+        [Test]
+        public void TranslateNumber9()
+        {
+            Translator translator = new Translator();
+            Assert.AreEqual("Foo", translator.Translate("9"));
+        }
+
+        [Test]
+        public void TranslateNumber10()
+        {
+            Translator translator = new Translator();
+            Assert.AreEqual("Bar*", translator.Translate("10"));
+        }
+
+        [Test]
+        public void TranslateNumber13()
+        {
+            Translator translator = new Translator();
+            Assert.AreEqual("Foo", translator.Translate("13"));
+        }
+
+
+        [Test]
+        public void TranslateNumber15()
+        {
+            Translator translator = new Translator();
+            Assert.AreEqual("FooBarBar", translator.Translate("15"));
+        }
+
+        [Test]
+        public void TranslateNumber21()
+        {
+            Translator translator = new Translator();
+            Assert.AreEqual("FooQix", translator.Translate("21"));
+        }
+
+        [Test]
+        public void TranslateNumber33()
+        {
+            Translator translator = new Translator();
+            Assert.AreEqual("FooFooFoo", translator.Translate("33"));
+        }
+
+        [Test]
+        public void TranslateNumber51()
+        {
+            Translator translator = new Translator();
+            Assert.AreEqual("FooBar", translator.Translate("51"));
+        }
+
+        [Test]
+        public void TranslateNumber53()
+        {
+            Translator translator = new Translator();
+            Assert.AreEqual("BarFoo", translator.Translate("53"));
+        }
+
+        [Test]
+        public void TranslateNumber101()
+        {
+            Translator translator = new Translator();
+            Assert.AreEqual("1*1", translator.Translate("101")); 
+        }
+
+        [Test]
+        public void TranslateNumber303()
+        {
+            Translator translator = new Translator();
+            Assert.AreEqual("FooFoo*Foo", translator.Translate("303")); 
+        }
+
+        [Test]
+        public void TranslateNumber105()
+        {
+            Translator translator = new Translator();
+            Assert.AreEqual("FooBarQix*Bar", translator.Translate("105"));
+        }
+
+        [Test]
+        public void TranslateNumber10101()
+        {
+            Translator translator = new Translator();
+            Assert.AreEqual("FooQix**", translator.Translate("10101"));
+        }
+
+    }
+}
---
+++ Translator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace FooBarQix
+{
+    internal class Translator
+    {
+        public Translator()
+        {
+        }
+
+        internal string Translate(string input)
+        {
+            int number = Convert.ToInt32(input);
+            string translated = "";
+
+            if (number % 3 == 0)
+                translated += "Foo";
+
+            if ((number % 5 == 0))
+                translated += "Bar";
+
+            if ((number % 7 == 0))
+                translated += "Qix";
+
+            foreach (char letter in input)
+            {
+                if (translated != "")
+                    if (letter == '0')
+                        translated += "*";
+
+                if (letter == '3')
+                    translated += "Foo";
+
+                if (letter == '5')
+                    translated += "Bar";
+
+                if (letter == '7')
+                    translated += "Qix";
+            }
+
+            if (translated == "")
+            {
+                foreach (char letter in input)
+                {
+                    if (letter == '0')
+                        translated += "*";
+                    else
+                        translated += letter;
+                }
+            }
+            return translated;
+        }
+    }
+}
