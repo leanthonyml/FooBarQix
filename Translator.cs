0ab0d4089c7a0b38898b02e3af8222efa5a42324
leanthonyml <40511671+leanthonyml@users.noreply.github.com>
2018-06-23 20:55:53 -0400

    Add files via upload

--- Translator.cs
+++ Translator.cs
@@ -8,10 +8,9 @@ namespace FooBarQix
         {
         }
 
-        internal string Translate(string input)
+
+        internal string BaseCases(int number, string translated)
         {
-            int number = Convert.ToInt32(input);
-            string translated = "";
 
             if (number % 3 == 0)
                 translated += "Foo";
@@ -22,21 +21,39 @@ namespace FooBarQix
             if ((number % 7 == 0))
                 translated += "Qix";
 
+            return translated;
+
+        }
+
+        internal string ExtraCases(string input, string translated)
+        {
             foreach (char letter in input)
             {
-                if (translated != "")
-                    if (letter == '0')
-                        translated += "*";
-
-                if (letter == '3')
-                    translated += "Foo";
-
-                if (letter == '5')
-                    translated += "Bar";
-
-                if (letter == '7')
-                    translated += "Qix";
+                translated = ExtraCasesInternal(letter, translated);
             }
+
+            return translated;
+        }
+
+        internal string ExtraCasesInternal(char letter, string translated)
+        {
+            if (translated != "" && letter == '0')
+                    translated += "*";
+
+            if (letter == '3')
+                translated += "Foo";
+
+            if (letter == '5')
+                translated += "Bar";
+
+            if (letter == '7')
+                translated += "Qix";
+
+            return translated;
+        }
+
+        internal string AsteriskCase(string input, string translated)
+        {
 
             if (translated == "")
             {
@@ -48,6 +65,20 @@ namespace FooBarQix
                         translated += letter;
                 }
             }
+            return translated;
+
+        }
+
+
+        internal string Translate(string input)
+        {
+            int number = Convert.ToInt32(input);
+            string translated = "";
+
+            translated = BaseCases(number, translated);
+            translated = ExtraCases(input, translated);
+            translated = AsteriskCase(input,translated);
+
             return translated;
         }
     }
